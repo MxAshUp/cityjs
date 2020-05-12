587d955deefef134858aebd371f625f0cf944ea4
Dustin Woods <pyrozzcom@gmail.com>
2020-05-12 08:09:43 -0700

    Named test

--- test/city-test.js
+++ test/city-test.js
@@ -1,7 +1,7 @@
 const describe  = require('kape');
 const {nearestCity} = require('../');
 
-describe('Name of Function', nearestCity, snapshot =>
+describe('nearestCity()', nearestCity, snapshot =>
   snapshot(
     [{latitude: 44.0618643, longitude: -121.3188065}],
     [{latitude: 41.3394978, longitude: -96.1462098}],
