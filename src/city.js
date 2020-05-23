24c2f2de856c1470081626657b6623cdc8709870
Dustin Woods <pyrozzcom@gmail.com>
2020-05-22 22:15:48 -0700

    Changed return signature for nearest city

--- src/city.js
+++ src/city.js
@@ -1,3 +1,9 @@
 import citiesTree from './cities';
 
-export const nearestCity = (point) => citiesTree.nearest(point, 1);
+export const nearestCity = (point) => {
+  const nearestNode = citiesTree.nearest(point, 1)[0];
+  return {
+    ...nearestNode[0],
+    distance: nearestNode[1]
+  }
+};
